44fe84dfa3875b55cd1328c4c5f9b36fb7a9340b
Scarlett-G05 <147156618+Scarlett-G05@users.noreply.github.com>
2025-06-06 00:28:23 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,107 +1,161 @@
-let play1;
-let bckgImg;
-let bubble;
-let play2;
-let d1;
-let d2;
-let score1;
-let score2;
-let bubs = []
-function preload(){
-  bckgImg = loadImage('bacground.jpg')
-}
+let sound;
+let sliderRate;
+let sliderVol;
+let sliderPan;
+let circPress;
+let wavePress;
+let recPress;
+let flarePress;
+let env;
+let fft;
+let volHistory = [];
+
+
 function setup() {
-  createCanvas(600, 500);
+  createCanvas(500, 500); 
+  angleMode(DEGREES);
+  //background(0); 
+  //sound = loadSound("forest_audio.mp3", loaded)
+  //waveButton = createButton("wave")
+  //waveButton.mousePressed(waveFunc)
   
+  env = new p5.Env();
+  env.setADSR(0.05,0.15,0.4,0.6)
+  env.setRange(0.7,0)
+  //env.play()
   
-  score1 = 0
-  score2 = 0
-  play1 = new Player1()
-  bubble = new bubbles()
-  play2 = new Player2()
+  wave = new p5.Oscillator()
+  wave.setType('sine')
+  wave.start()
+  wave.amp(env)
+  wave.freq(440)
   
-  //d1 = dist(bubble.x,bubble.y,play1.x,play1.y)
-  //d2 = dist(bubble.x,bubble.y,play2.x,play2.y)
+  waveButton = createButton("wave")
+  waveButton.mousePressed(waveFunc)  
+  
+  circButton =createButton("circle")
+  circButton.mousePressed(circ)
+  
+  flareButton =createButton("flare")
+  flareButton.mousePressed(flareFunc)
+  
+  recButton =createButton("rectangle")
+  recButton.mousePressed(recFunc)
+  
+
+  
+  sliderVol = createSlider(0,1,0.65,0.01)
+  //sliderPan = createSlider(-1,1,0,0.01)
+  sliderRate = createSlider(0,4,1,0.01)
+  amp = new p5.Amplitude()
+  fft = new p5.FFT()
+  
 }
 
 
-//game idea
-//2 player
-//side moving game. Players have to jump up to randomly generated bubbles and collect points. player with the most points at end of 60 seconds wins
-function keyPressed(){
-  if (key =='w'){
-    play1.jump()
-  }
-  if(key=='a'){
-    play1.move_L()
-  }
-  if(key=='d'){
-    play1.move_R()
-  }
-  if (key =='h'){
-    play2.jump()
-  }
-  if(key=='b'){
-    play2.move_L()
-  }
-  if(key=='m'){
-    play2.move_R()
-  }
+function preload(){
+  sound = loadSound("forest_audio.mp3", loaded)
 }
 
-//maybe put bubbles in a  list
+function waveFunc(){
+  circPress = false
+  wavePress=true
+  flarePress=false
+  recPress=false
+}
+
+function circ(){
+  circPress=true
+  wavePress=false
+  flarePress=false
+  recPress=false
+}
+
+function flareFunc(){
+  circPress=false
+  wavePress=false
+  flarePress=true
+  recPress=false  
+}
+
+function recFunc(){
+  circPress=false
+  wavePress=false
+  flarePress=false
+  recPress=true 
+}
+//need to find a way to get 
+
+function loaded(){
+  sound.loop()
+}
+//A real time audio visualiser, with 4 different effects
+
 
 
 function draw() {
-  background(bckgImg)
-  fill(255,120,120)
-  play1.show();
-  push()
-  fill(0,100,255)
-  play1.move();
-  play2.show();
-  pop()
-  play2.move();
+  background(0); 
+  let loud = amp.getLevel()
+  let louder = loud*4500
+  let loudness = loud*10
+  volHistory.push(loudness)
+  
+  let spectrum = fft.analyze();
+  console.log(spectrum)
+    
+  sound.setVolume(sliderVol.value())
+  //sound.pan(sliderPan.value())
+  
+  sound.rate(sliderRate.value())
+  let len = sound.duration()
+  let rn = sound.currentTime()
   
   
   
-  if (random(1)<0.01){
-    bubs.push(new bubbles())
+  if(circPress == true){
+    ellipse(width/2,height/2,400, louder)
   }
-  
-  for(let b of bubs){
-    b.show()
+  else if(wavePress == true){
+    stroke(200)
+    noFill()
+    beginShape();
+    for(var i = 0; i<volHistory.length;i++){
+      var y = map(volHistory[i],0,1,height,0);    
+      vertex(i,y);
+    }
+    endShape();  
+    if(volHistory.length>width){
+      volHistory.splice(0,1)
+    }
   }
+  else if(flarePress == true){
+    stroke(200)
+    noFill()
+    translate(width/2,height/2)
+    beginShape();
+    for(var p = 0; p<360;p++){
+      var r = map(volHistory[p],0,1,10,100);
+      var x = r*cos(p);
+      var z = r*sin(p);   
+      vertex(x,z);
+    }
+  endShape(); 
   
+  if(volHistory.length>360){
+    volHistory.splice(0,1)
+  }
+    
+  }
+  else if(recPress == true){
+    for(let f = 0;f<spectrum.length;f++)
+      rect(0,0,spectrum[f],louder)
+    
+  }
+          
+          
 
-  if (play1.x == bubbles.x) {
-    if(play1.y == bubbles.y);{
-      score1+=1
-    }
-  }  
-  if (play2.x == bubbles.x) {
-    if(play2.y == bubbles.y);{
-      score2+=1
-    }
-  }
   
-  textSize(40)
-  text(score1,40,40)
-  text(score2,430,40)
-  //if player collides with bubble add a point to the count
-  //pop bubs from list when colided with
-  //show points in rect on side of screen
-  //put 60 second count down in
-  if(second> 60){
-    if(score1>score2){
-      text('congrats player 1 won!',200,200)
-    }
-    else if(score2>score1){
-      text('congrats player 2 won!' ,200,200)
-    }
-    else{
-      text('it was a draw',200,200)
-    }
-  }
- 
+  //console.log(volHistory)
+    
 }
+
